f345b3e54b76c1254cf75dd2671419da434b008d
xiafengsu <xxxfengsu@163.com>
2024-07-23 19:33:47 +0545

    fix: add inedx.css and move script in preload.js

--- preload.js
+++ preload.js
@@ -2,4 +2,20 @@ const dayjs = require("dayjs");
 
 window.addEventListener("DOMContentLoaded", () => {
   window.dayjs = dayjs;
+
+  function updateTime() {
+    if (window.dayjs) {
+      const dateElement = document.getElementById("date");
+      const currentDate = window.dayjs().format("YYYY-MM-DD HH:mm:ss");
+      dateElement.innerText = `${currentDate}`;
+    } else {
+      console.error("dayjs is not available");
+    }
+  }
+
+  // 初始时间更新
+  updateTime();
+
+  // 每秒更新一次时间
+  setInterval(updateTime, 1000);
 });
